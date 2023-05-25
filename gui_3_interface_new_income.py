6bc9233409a05d790801dc4a67119ec67204d357
AlvaradoCPY <alvaradocpy@gmail.com>
2023-05-25 11:35:32 -0700

    Default Frame, personal data

--- gui_3_interface_new_income.py
+++ gui_3_interface_new_income.py
@@ -17,8 +17,54 @@ class GUI_root:
 
 
 class GUI_framePersonalData:
-    def __init__(self, frame: tk.Frame) -> None:
+    def __init__(self, frame: tk.Frame, data: dict) -> None:
         self.frame = frame
+
+        # ci
+        self.label_ci: tk.Label = tk.Label(self.frame, text="CÉDULA")
+        self.label_ci.grid(row=0, column=0, sticky="WNS")
+
+        self.entry_ci: tk.Entry = tk.Entry(self.frame)
+        self.entry_ci.grid(row=1, column=0, sticky="WENS")
+
+        # fullname
+        self.label_full_name: tk.Label = tk.Label(
+            self.frame, text="NOMBRE COMPLETO")
+        self.label_full_name.grid(row=2, column=0, sticky="WNS")
+
+        self.entry_full_name: tk.Entry = tk.Entry(self.frame)
+        self.entry_full_name.grid(row=3, column=0, sticky="WENS")
+
+        # nacionality
+        self.label_nacionality: tk.Label = tk.Label(
+            self.frame, text="NACIONALIDAD")
+        self.label_nacionality.grid(row=4, column=0, sticky="WNS")
+
+        self.entry_nacionality: tk.Entry = tk.Entry(self.frame)
+        self.entry_nacionality.grid(row=5, column=0, sticky="WENS")
+
+        # birthday
+        self.label_birthday: tk.Label = tk.Label(
+            self.frame, text="FECHA DE NACIMIENTO")
+        self.label_birthday.grid(row=6, column=0, sticky="WNS")
+
+        self.entry_birthday: tk.Entry = tk.Entry(self.frame)
+        self.entry_birthday.grid(row=7, column=0, sticky="WENS")
+
+        # age
+        self.label_age: tk.Label = tk.Label(self.frame, text="EDAD")
+        self.label_age.grid(row=8, column=0, sticky="WNS")
+
+        self.entry_age: tk.Entry = tk.Entry(self.frame)
+        self.entry_age.grid(row=9, column=0, sticky="WENS")
+
+        # gender
+        self.label_gender: tk.Label = tk.Label(self.frame, text="GENERO")
+        self.label_gender.grid(row=10, column=0, sticky="WNS")
+
+        self.entry_gender: tk.Entry = tk.Entry(self.frame)
+        self.entry_gender.grid(row=11, column=0, sticky="WENS")
+
 
 class GUI_addWorker(GUI_root):
     def __init__(self, root: tk.Tk) -> None:
@@ -60,31 +106,34 @@ class GUI_addWorker(GUI_root):
         self.frame_container: tk.Frame = tk.Frame(self.root)
         self.frame_container.grid(row=1, column=0, sticky="WENS")
 
-        self.frame_container.grid_rowconfigure(0 , weight=1)
+        self.frame_container.grid_rowconfigure(0, weight=1)
         self.frame_container.grid_columnconfigure(0, weight=1)
 
         self.frame_buttons: tk.Frame = tk.Frame(self.root)
         self.frame_buttons.grid(row=2, column=0, sticky="WENS")
 
         self.frame_buttons.grid_rowconfigure(0, weight=1)
-        self.frame_buttons.grid_columnconfigure((0,1), weight=1)
+        self.frame_buttons.grid_columnconfigure((0, 1), weight=1)
 
         # title
-        self.label_title: tk.Label = tk.Label(self.frame_title, text="Cambiante")
+        self.label_title: tk.Label = tk.Label(
+            self.frame_title, text="Cambiante")
         self.label_title.grid(row=0, column=0, sticky="WENS")
 
         # changable frame
         self.setDefaultFrame()
 
         # buttons
-        self.button_last_frame: tk.Button = tk.Button(self.frame_buttons, text="Anterior")
+        self.button_last_frame: tk.Button = tk.Button(
+            self.frame_buttons, text="Anterior")
         self.button_last_frame.grid(row=0, column=0)
 
-        self.button_next_frame: tk.Button = tk.Button(self.frame_buttons, text="Siguiente")
+        self.button_next_frame: tk.Button = tk.Button(
+            self.frame_buttons, text="Siguiente")
         self.button_next_frame.grid(row=0, column=1)
 
     def setDefaultFrame(self):
-        GUI_framePersonalData(self.frame_container)
+        GUI_framePersonalData(self.frame_container, self.worker_data)
 
 
 class GUI_workerForm:
