7b516c32b198d7c1aee60edca1b62a5ec9872e4d
AlvaradoCPY <alvaradocpy@gmail.com>
2023-05-25 09:04:27 -0700

    Better naming

--- gui_2_main_menu.py
+++ gui_2_main_menu.py
@@ -6,7 +6,7 @@ from app_global_variables import guiConfig, dbPath
 from helpers.import_data import ImportData
 from helpers.format import formatDate, formatAddMissingZero
 from helpers.search_engine import SearchEngine
-from gui_3_interface_worker import GUI_workerForm
+from gui_3_interface_new_income import GUI_workerForm
 
 
 class GUI_root:
